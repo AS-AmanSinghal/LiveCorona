30c094f269cedd59658958bb1b35585ecbd1b08a
Aman Singhal <amansinghal.it1@gmail.com>
2020-03-26 11:14:17 +0530

    Create Splash Activity

---
+++ app/src/main/java/com/weshowedup/livecorona/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.weshowedup.livecorona;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, TIME);
+    }
+}
